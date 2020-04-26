efd996f935121cea982f58cbe046d11d7c4f1332
Logan McGuire <lrmcguire93@gmail.com>
2020-04-26 08:39:38 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -19,6 +19,7 @@ def get_japanese_emoticon(path, emoticon)
   library = {};
   library = load_library(path);
   library.map { |emotion| 
+    binding.pry; 
     if emotion[:english] == emoticon
       return emotion[:japanese];
     end
