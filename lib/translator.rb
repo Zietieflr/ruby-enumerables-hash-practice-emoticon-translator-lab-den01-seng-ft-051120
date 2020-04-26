30f6e54f2042dd284fc3fc5e52b92dfeffceb7c0
Logan McGuire <lrmcguire93@gmail.com>
2020-04-26 08:24:12 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,11 +1,22 @@
 # require modules here
+require "yaml";
+require "pry";
 
-def load_library
+def load_library(path)
   # code goes here
+  library = {};
+  yaml_h = YAML.load_file(path);
+  yaml_h.map { |descriptor| 
+    key = descriptor[0];
+    value = { english: descriptor[1][0], japanese: descriptor[1][1] };
+    library.store(key, value);
+  }
+  library; 
 end
 
-def get_japanese_emoticon
+def get_japanese_emoticon(path, emoticon)
   # code goes here
+  
 end
 
 def get_english_meaning
